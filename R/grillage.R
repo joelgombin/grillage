092efe01094e925022a8df333296d7ba763ffd3d
Joel Gombin <joel.gombin@gmail.com>
2020-11-08 00:15:30 +0100

    - FIX: extract polygons in `gr_get_enveloppe` - FEAT: new function `gr_list_to_bv`

--- R/grillage.R
+++ R/grillage.R
@@ -74,5 +74,65 @@ gr_get_enveloppe <- function(code_insee) {
   correspondance_table <- read.csv("https://gist.githubusercontent.com/Marc-marc-marc/07b0d74e63be682505b49488221b1bb9/raw/d1ec095657796a27bd37efc8e378e1836acdd0a5/gistfile1.txt", sep = "\t", col.names = c("id", "insee"), stringsAsFactors = FALSE)
   id <- correspondance_table[correspondance_table$insee %in% code_insee, "id"]
   shape <- sf::st_read(glue::glue("http://polygons.openstreetmap.fr/get_geojson.py?id={id}&params=0"))
+  shape <- sf::st_collection_extract(shape, "POLYGON")
   return(shape)
+}
+
+#' Get polling stations shapes from the voters register
+#'
+#' @param path path to the voters register (preferably in the REU format)
+#' @param code_insee code INSEE of the commune
+#' @param n a vector, number of cells used (rows and columns)
+#' @param confidence_level threshold under which geolocalisation is not to be trusted
+#' @param numero_bv column containing polling station code/number
+#' @param numero_voie column containing the number in the street
+#' @param libelle_voie column containing the name of the street
+#' @param code_postal column containing the zip code
+#' @param ville column containing the name of the city
+#'
+#' @return an sf object containing the (estimated) shape of the polling stations
+#' @export
+#'
+
+gr_list_to_bv <- function(path, code_insee, n = c(50, 50), confidence_level = 0.6,
+                        numero_bv = `numéro du bureau de vote`,
+                        numero_voie = NumeroVoie,
+                        libelle_voie = LibelleVoie,
+                        code_postal = `Code postal`,
+                        ville = VilleLocalite) {
+  if (stringr::str_ends(path, coll(".csv"))) {
+    listes <- readr::read_csv2(path)
+  } else if (str_ends(path, coll(".xls"))) {
+    listes <- readxl::read_xls(path)
+  } else if (str_ends(path, coll(".xlsx"))) {
+    listes <- readxl::read_xlsx(path)
+  }
+
+  listes <- listes %>%
+    select({{numero_bv}}, {{numero_voie}}, {{libelle_voie}}, {{code_postal}}, {{ville}}) %>%
+    mutate({{code_postal}} := str_pad({{code_postal}}, 5, "left", "0")) %>%
+    distinct()
+
+  listes <- listes %>%
+    mutate({{libelle_voie}} := str_replace({{libelle_voie}}, "BD ", "Boulevard "),
+           {{libelle_voie}} := str_replace({{libelle_voie}}, "R ", "Rue "),
+           {{libelle_voie}} := str_replace({{libelle_voie}}, "AV ", "Avenue "),
+           {{libelle_voie}} := str_replace({{libelle_voie}}, "CHE ", "Chemin "),
+           {{libelle_voie}} := str_replace({{libelle_voie}}, "IMP ", "Impasse ")) %>%
+    mutate(adresse = paste0({{numero_voie}}, " ", {{libelle_voie}}),
+           code_insee = code_insee) %>%
+    banR::geocode_tbl(adresse, code_insee) %>%
+    filter(!is.na(longitude), !is.na(latitude)) %>%
+    st_as_sf(coords = c("longitude", "latitude")) %>%
+    st_set_crs(4326)
+
+  adresses <- listes %>%
+    filter(result_score >= confidence_level) %>%
+    distinct(result_id, .keep_all = TRUE)
+
+  bv <- adresses %>%
+    grillage::gr_create_shape({{numero_bv}}, n = n, code_insee = code_insee) %>%
+    mutate(bureau_vote_id = paste0(code_insee, stringr::str_pad(polygon, width = 4, pad = "0", side = "left")))
+
+  return(bv)
 }
